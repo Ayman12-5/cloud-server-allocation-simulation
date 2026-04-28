5490cc32857b0f8af7a9f07e0dd441f80a5700f6
Ayman12-5 <aymanhcm55556@gmail.com>
2026-04-28 03:41:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("Sim.html")
+    return render_template("index.html")
 
 
 @app.route("/run-simulation", methods=["POST"])
